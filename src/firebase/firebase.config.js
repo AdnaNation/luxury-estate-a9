952f317f4d156d9eaa694a7fe0f2997153a92817
AdnaNation <azizulislamadnan@gmail.com>
2024-04-13 12:59:41 +0600

    nav bar added

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAY4G97z2WUQqR8PyjOF2KonikxWvbmU98",
+  authDomain: "assingment09-luxury-estate.firebaseapp.com",
+  projectId: "assingment09-luxury-estate",
+  storageBucket: "assingment09-luxury-estate.appspot.com",
+  messagingSenderId: "1027895998301",
+  appId: "1:1027895998301:web:eacde660e81edcc152dab4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
