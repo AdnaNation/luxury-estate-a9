b85ec1ce3b87e89d3407cb3041e734bff0ae1578
AdnaNation <azizulislamadnan@gmail.com>
2024-04-13 12:22:25 +0600

    primary setup done

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { MdOutlineRealEstateAgent } from "react-icons/md";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-poppins">
+        <MdOutlineRealEstateAgent></MdOutlineRealEstateAgent> Vite + React
+      </h1>
+    </>
+  );
+}
+
+export default App;
